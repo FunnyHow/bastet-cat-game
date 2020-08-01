559ef00104bde6b71d201f8f48d122d84c81552b
Robin Tregaskis <lokidokicoki@gmail.com>
2020-08-01 16:38:27 +0100

    health bar innit

---
+++ Bastet cat game/Assets/GUI/Health.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    public int numberofAnkhs;
+    public Image[] ankhs;
+    public Sprite fullAnkh;
+    public Sprite emptyAnkh;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < ankhs.Length; i++)
+        {
+            if (i < numberofAnkhs)
+            {
+                ankhs[i].enabled = true;
+            }
+            else { 
+                ankhs[i].enabled = false;
+            }
+        } 
+    }
+}
