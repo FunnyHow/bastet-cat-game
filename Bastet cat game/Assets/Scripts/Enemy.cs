c425180bd263ffd379fc57fdacdfdad825fd1269
Robin Tregaskis <lokidokicoki@gmail.com>
2020-08-02 15:01:45 +0100

    human anims and death state

--- Bastet cat game/Assets/Scripts/Enemy.cs
+++ Bastet cat game/Assets/Scripts/Enemy.cs
@@ -6,6 +6,7 @@ public class Enemy : MonoBehaviour
 {
     public int maxHealth = 100;
     public int currentHealth;
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,7 @@ public class Enemy : MonoBehaviour
         currentHealth -= damage;
 
         //play hurt anim
+        animator.SetTrigger("Hurt");
 
         if(currentHealth <= 0)
         {
@@ -26,8 +28,11 @@ public class Enemy : MonoBehaviour
     void Die() {
         Debug.Log("Aieeee!");
         // play death anim
+        animator.SetBool("IsDead", true);
 
         //disable
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
     }
 
 }
