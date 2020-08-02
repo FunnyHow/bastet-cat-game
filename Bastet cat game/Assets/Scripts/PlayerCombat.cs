0b4abde70a2b4020ac360a22c591f225606cace5
Robin Tregaskis <lokidokicoki@gmail.com>
2020-08-02 13:13:40 +0100

    add tag to human, add tag detection to combat script. TODO: add enemy script

--- Bastet cat game/Assets/Scripts/PlayerCombat.cs
+++ Bastet cat game/Assets/Scripts/PlayerCombat.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerCombat : MonoBehaviour
 {
     public Animator animator;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+
+
 
     // Start is called before the first frame update
     void Attack()
@@ -12,8 +18,22 @@ public class PlayerCombat : MonoBehaviour
         //play anim
         animator.SetTrigger("Attack");
         //detect enemy collsion
+        Collider2D[] hitObjects= Physics2D.OverlapCircleAll(attackPoint.position, attackRange);
         // do damage
-        
+
+        foreach (Collider2D item in hitObjects) {
+            if (item.tag == "Enemy")
+            {
+                Debug.Log("hit this " + item.name);
+            }
+        }
+
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 
     // Update is called once per frame
