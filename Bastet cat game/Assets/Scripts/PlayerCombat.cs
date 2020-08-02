5a93f350d5ba4137ee0032c5e6548499014f7298
Robin Tregaskis <lokidokicoki@gmail.com>
2020-08-02 12:23:32 +0100

    include combat script

---
+++ Bastet cat game/Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Attack()
+    {
+        //play anim
+        animator.SetTrigger("Attack");
+        //detect enemy collsion
+        // do damage
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl)) {
+            Attack();
+        }    
+    }
+}
